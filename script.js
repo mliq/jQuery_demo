910d3cad860d7b1a5d9ce72c04fdee3ef3cec298
mliq <liquori@gmail.com>
2015-04-21 22:03:18 -0500

    Centering Refinements

--- script.js
+++ script.js
@@ -7,15 +7,18 @@ $(document).ready(function () {
 
     thing.css('z-index', 1);
     thing.children('.zoomText').css('font-size', '60px')
+    thing.children('.zoomText').css('font-family', 'BatmanForeverAlternate')
     thing.children('.zoomText').css('color', 'yellow')
 
     // this is where the popping out effect happens
-    $(this).animate({ height: "400", width: "375", left: "-=55", top: "-=55" }, "fast");
+    $(this).animate({ height: "400", width: "375", left: "-=85", top: "-=55" }, "fast");
   }, 
   function() {
     thing.css("z-index", 0);
+    $(this).animate({ height: "200", width: "187.5", left: "+=85", top: "+=55" }, "fast");
     thing.children('.zoomText').css('font-size', '20px')
-    $(this).animate({ height: "200", width: "187.5", left: "+=55", top: "+=55" }, "fast");
+    thing.children('.zoomText').css('font-family', 'monospace')
+    thing.children('.zoomText').css('color', 'orange')
   }
   );
 });

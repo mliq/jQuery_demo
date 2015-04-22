07623ec9730d1c835a2964ad148e4169f4e49442
mliq <liquori@gmail.com>
2015-04-21 22:46:32 -0500

    fade in drama

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 var thing;
+var textThing;
 
 $(document).ready(function () {
 
   $(".zoom").hover(function() {
     thing = $(this).closest('.zoom');
+    textThing = thing.children('.zoomText');
 
     thing.css('z-index', 1);
-    thing.children('.zoomText').css('font-size', '60px')
-    thing.children('.zoomText').css('font-family', 'BatmanForeverAlternate')
-    thing.children('.zoomText').css('color', 'yellow')
+    $(this).animate({ height: "400", width: "375", left: "-=85", top: "-=55" }, "fast");
+    textThing.hide();
+
+    textThing.css('font-size', '60px').css('font-family', 'BatmanForeverAlternate').css('color', 'yellow');
+    textThing.fadeIn(800);
 
     // this is where the popping out effect happens
-    $(this).animate({ height: "400", width: "375", left: "-=85", top: "-=55" }, "fast");
   }, 
   function() {
     thing.css("z-index", 0);
     $(this).animate({ height: "200", width: "187.5", left: "+=85", top: "+=55" }, "fast");
-    thing.children('.zoomText').css('font-size', '20px')
-    thing.children('.zoomText').css('font-family', 'monospace')
-    thing.children('.zoomText').css('color', 'orange')
+    
+
+    textThing.css('font-size', '20px');
+    textThing.css('font-family', 'monospace');
+    textThing.css('color', 'orange');
   }
   );
 });

e7da10d59a4df29d0ce8c6c16586c1614bc21a46
mliq <liquori@gmail.com>
2015-04-21 21:42:38 -0500

    Absolute positioning

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
+var thing;
+
 $(document).ready(function () {
 
-  $("#batCat").hover(function() {
-    $(this).closest("img").css("z-index", 1);
+  $(".zoom").hover(function() {
+    thing = $(this).closest('.zoom');
+
+    thing.css('z-index', 1);
+    thing.children('.zoomText').css('font-size', '60px')
+    thing.children('.zoomText').css('color', 'yellow')
+
     // this is where the popping out effect happens
     $(this).animate({ height: "400", width: "375", left: "-=55", top: "-=55" }, "fast");
-  }, function() {
-        $(this).closest("img").css("z-index", 0);
-        $(this).animate({ height: "200", width: "187.5", left: "+=55", top: "+=55" }, "fast");
-        }
-      );
-  });
+  }, 
+  function() {
+    thing.css("z-index", 0);
+    thing.children('.zoomText').css('font-size', '20px')
+    $(this).animate({ height: "200", width: "187.5", left: "+=55", top: "+=55" }, "fast");
+  }
+  );
+});
 
 
